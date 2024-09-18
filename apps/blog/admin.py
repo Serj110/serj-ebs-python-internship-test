c42ee348a2de69d937fd5e60681b92455ef29f0f
Serj110 <sergheipetrov110@gmail.com>
2024-09-18 12:33:59 +0300

    task 2
    corrections

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-
-from apps.blog.models import Blog, Category
+from .models import Blog, Category
 
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_enabled')
+    list_display = ('title', 'enabled', 'posted', 'category')  # Display these fields
+    list_filter = ('enabled', 'category')  # Filters
+    search_fields = ('title',)  # Search functionality
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')  # Display fields for categories
+    prepopulated_fields = {'slug': ('title',)}  # Auto-populate slug from title
 
 
 admin.site.register(Blog, BlogAdmin)
-admin.site.register(Blog)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
+
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Blog(models.Model):
@@ -14,7 +15,7 @@ class Blog(models.Model):
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    enabled = models.BooleanField(default=True)   # New field to indicate if the blog is published or unpublished
-    name = models.CharField(max_length=255)
-    is_enabled = models.BooleanField(default=True)
+    enabled = models.BooleanField(default=True)  # Indicates if the blog is published
 
+    def __str__(self):
+        return self.title
